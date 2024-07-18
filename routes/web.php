b8904d3d612172e095927ffad9da90d7e0a8bc55
Nsengiyumva Wilberforce <nsengiyumvawilberforce@gmail.com>
2024-07-18 09:43:29 +0300

    new update

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect('/login');
+    return redirect('/admin');
 });
 
 Route::get('/dashboard', function () {
